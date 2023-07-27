463367bb5dce6a05af65930683996f294fce648a
Helder <helderlucau@gmail.com>
2023-07-27 09:06:20 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      <div>
+        {actors.map((actor) => (
+          <div key={actor.name}>
+            <h2>{actor.name}</h2>
+            <ul>
+            {actor.movies.map((actorMovies) => (
+              <li key={actorMovies}>{actorMovies}</li>
+            ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <h1>Directors Page</h1>
+  <div>
+  {directors.map((director) => (
+    <div key={director.name}>
+    <h2>{director.name}</h2>
+    <ul>
+    {director.movies.map((directorMovie) => (
+      <li key={directorMovie}>{directorMovie}</li>
+    ))}
+    </ul>
+    </div>
+  ))}
+  </div>
+  </div>;
 }
 
 export default Directors;
